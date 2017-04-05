1a1b3f55a75556947a4ed9d3876016d6178508b2
skill208 <skill208@naver.com>
2017-04-05 13:11:40 +0900

    first commit

--- MyApplication/app/src/main/java/com/soft/yss/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/soft/yss/myapplication/MainActivity.java
@@ -1,13 +1,13 @@
-package com.example.kcj.myapplication;
+package com.soft.yss.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class AndroidLecture extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_android_lecture);
+        setContentView(R.layout.activity_main);
     }
 }
